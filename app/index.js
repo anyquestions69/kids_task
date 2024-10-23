38ea40751b05ae5f59f4d31757a2bda0a6224c18
public_hysteria <juuzodes@yandex.ru>
2024-10-23 21:36:00 +0300

    pause

--- app/index.js
+++ app/index.js
@@ -1,6 +1,22 @@
 const express= require('express')
+var basicAuth = require('basic-auth');
 const { exec, spawn } = require('node:child_process');
+var bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var auth = function(username, password) {
+    return function(req, res, next) {
+      var user = basicAuth(req);
+  
+      if (!user || user.name !== username || user.pass !== password) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        return res.send(401);
+      }
+  
+      next();
+    };
+  };
 app.get('/',(req,res)=>{
     const url = req.query.url
     exec('curl '+url, (err, stdout, stderr) => {
@@ -11,5 +27,11 @@ app.get('/',(req,res)=>{
         console.log(stdout);
         return res.send(stdout)
       });
+})
+app.post('/issue', (req,res)=>{
+    return res.send(JSON.stringify(req.body))
+})
+app.get('/admin', auth('admin', 'iowe3-03ndin2in13'), (req,res)=>{
+    return res.send('admin')
 })
 app.listen(3000)
